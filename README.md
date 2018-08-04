a76269095ebc4ba5635acc55763309c012bc92c7
duke79 <pulkitsingh01@gmail.com>
2018-08-04 10:58:06 +0530

    sidenav code

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
-# responsive-react-sidebar
-Responsive Sidebar in React
+Responsive React Sidebar  [![npm version](https://badge.fury.io/js/responsive-react-sidebar.svg)](http://badge.fury.io/js/react-sidebar)
+=============
+
+Responsive React Sidebar is a sidebar component for React with responsive design.
+
+Installation
+------------
+`npm install responsive-react-sidebar`
+
+Getting started
+-----------------
+
+```jsx
+import React from 'react';
+import ResponsiveSidebar from 'responsive-react-sidebar';
+
+class App extends React.Component {
+  onSideNavHidden() {
+        //Probably switch the z-index of the rest of the page in case it's fixed
+        //$("." + styles["Wrapper"]).css("z-index", "");
+  }
+
+  render() {
+    return (
+       <ResponsiveSidebar
+            onScrimClick={this.onSideNavHidden.bind(this)}
+            menu={
+              header: {
+                  "type": "item",
+                  "value": "Vilokan Labs",
+                  "link": "/"
+                },
+              container: [
+                  {
+                    "type": "item",
+                    "value": "Project",
+                    "link": "/project"
+                  },
+                  {
+                    "type": "submenu",
+                    "value": "Issues",
+                    "link": "",
+                    "items": [                    
+                        {
+                            "type": "subitem",
+                            "value": "Service Desk",
+                            "link": "/service_desk"
+                        },
+                        {
+                            "type": "subitem",
+                            "value": "Milestones",
+                            "link": "/milestones"
+                        }
+                      ]
+                  },
+                  {
+                      "type": "item",
+                      "value": "Members",
+                      "link": "/members"
+                  },
+                  {
+                      "type": "item",
+                      "value": "Wiki",
+                      "link": "/wiki"
+                  }            
+              ],
+              footer: {
+                  "type": "item",
+                  "value": "<< Collapse Sidebar",
+                  "link": ""
+              }
+              } 
+        />
+    );
+  }
+}
+
+export default App;
+```
--- src/main.js
+++ src/main.js
@@ -1,48 +1,204 @@
-import React from 'react';
-import PropTypes from 'prop-types';
- 
-const propTypes = {
-    label: PropTypes.string.isRequired,
-    onChange: PropTypes.func.isRequired,
-    styles: PropTypes.object
-}
- 
-const defaultProps = {
-    styles: {
-        label: {
-            fontFamily: 'Comic Sans MS',
-            color: 'green'
-        },
-        input: {
-            background: '#ddd',
-            border: '1px solid red'
+import { NavLink } from "react-router-dom";
+import React, { Component } from 'react';
+import $ from 'jquery'
+import styles from './main.css'
+import fa_styles from '../../lib/font-awesome/css/font-awesome.min.css';
+import '../../lib/styleUnmangled/Router.css'
+
+class ResponsiveSidebar extends Component {
+    state = {
+    }
+
+    componentDidMount() {
+        var activeSubItem = $("." + styles.MySideNav_SubItem + ".active");
+        if (typeof (activeSubItem) !== 'undefined') {
+            var subItemsContainer = activeSubItem.parent();
+            subItemsContainer.show(100);
+
+            var subMenuTitleCaretDown = subItemsContainer.parent().find("." + styles.MySideNav_CaretDown);
+            subMenuTitleCaretDown.toggleClass(styles["down"]);
+        }
+
+        $("." + styles.MySideNav_Brand + ", ." + styles.MySideNav_SubItem + ", ." + styles.MySideNav_Item + ", ." + styles.MySideNav_Scrim).click(function () {
+            this.hide();
+        }.bind(this));
+    }
+
+    onSubMenuClick(e) {
+        var subMenuItemsContainer = $(e.currentTarget).find("." + styles.MySideNav_SubItems_Container);
+        var subMenuTitleCaretDown = $(e.currentTarget).find("." + styles.MySideNav_CaretDown);
+        if ($(e.target).hasClass(styles.MySideNav_SubTitle) || $(e.target).hasClass(styles.MySideNav_CaretDown)) {
+            if (subMenuItemsContainer.css("display") === "none")
+                // subMenuItemsContainer.css("display", "block");
+                subMenuItemsContainer.show(100);
+            else
+                // subMenuItemsContainer.css("display", "none");
+                subMenuItemsContainer.hide(100)
+
+            subMenuTitleCaretDown.toggleClass(styles["down"]);
         }
     }
-}
- 
-class BoilerplateComponent extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleChange = this.handleChange.bind(this);
+
+    display() {
+        $("." + styles.MySideNav_Wrapper).removeClass(styles.Hidden);
+        $("." + styles.MySideNav_Scrim).removeClass(styles.Hidden);
     }
- 
-    handleChange(e) {
-        this.props.onChange(e.target.value);
+
+    hide() {
+        $("." + styles.MySideNav_Wrapper).addClass(styles.Hidden);
+        $("." + styles.MySideNav_Scrim).addClass(styles.Hidden);
+        this.props.onScrimClick();
     }
-     
+
+    getItems() {
+        var elems = []
+        var header = "";
+        var footer = "";
+        var container_elems = []
+
+        if (typeof (this.props.menu.header) !== "undefined") {
+            if (this.props.menu.header.link !== "") {
+                elems.push(
+                    <div className={styles.MySideNav_Header}>
+                        <NavLink exact to={this.props.menu.header.link} className={styles.MySideNav_Brand}>{this.props.menu.header.value}</NavLink>
+                    </div>
+                );
+            }
+            else {
+                elems.push(
+                    <div className={styles.MySideNav_Header}>
+                        <div className={styles.MySideNav_Brand}>{this.props.menu.header.value}</div>
+                    </div>
+                );
+            }
+        }
+
+        this.props.menu.container.forEach(element => {
+            if (element.type === "item") {
+                if (element.link !== "") {
+                    container_elems.push(
+                        <NavLink exact to={element.link} className={styles.MySideNav_Item}>{element.value}</NavLink>
+                    );
+                } else {
+                    container_elems.push(
+                        <div className={styles.MySideNav_Item}>{element.value}</div>
+                    );
+                }
+            }
+            if (element.type === "submenu") {
+                var subMenuTitle = {};
+                if (element.link !== "") {
+                    subMenuTitle = <NavLink exact to={element.link} className={"nav-text " + styles.MySideNav_SubTitle}>{element.value}</NavLink>
+                } else {
+                    subMenuTitle = <div to={element.link} className={"nav-text " + styles.MySideNav_SubTitle} >{element.value}
+                        <i className={fa_styles["fa"] + " " + fa_styles["fa-caret-down"] + " " + styles.MySideNav_CaretDown + " " + styles["rotate"]} />
+                    </div>
+                }
+
+                var subelems = [];
+                element.items.forEach(subitem => {
+                    if (subitem.link !== "") {
+                        subelems.push(
+                            <NavLink exact to={subitem.link} className={styles.MySideNav_SubItem}>{subitem.value}</NavLink>
+                        );
+                    } else {
+                        subelems.push(
+                            <div className={styles.MySideNav_SubItem}>{subitem.value}</div>
+                        );
+                    }
+                });
+                container_elems.push(<div className={styles.MySideNav_SubMenu} onClick={((e) => this.onSubMenuClick(e))}>
+                    {subMenuTitle}
+                    <div className={styles.MySideNav_SubItems_Container}>
+                        {subelems}
+                    </div>
+                </div>);
+            }
+        });
+
+        elems.push(
+            <div className={styles.MySideNav_Container}>
+                {container_elems}
+            </div>);
+
+        if (typeof (this.props.menu.footer) !== "undefined") {
+            if (this.props.menu.footer.link !== "") {
+                elems.push(
+                    <NavLink exact to={this.props.menu.footer.link} className={styles.MySideNav_Footer}>{this.props.menu.footer.value}</NavLink>
+                );
+            } else {
+                elems.push(
+                    <div className={styles.MySideNav_Footer}>{this.props.menu.footer.value}</div>
+                );
+            }
+        }
+
+        return elems;
+    }
+
     render() {
-        const styles = this.props.styles || {};
- 
-        return (
-            <div>
-                <label style={styles.label}>{this.props.label}</label>
-                <input type="text" style={styles.input} onChange={this.handleChange} />
-            </div>
-        );
+        // var urlParts = window.location.href.split("/");
+        var items = this.getItems()
+
+        return <div className={styles.MySideNav_Wrapper + " " + styles.Hidden}>
+            <div className={styles.MySideNav_Scrim + " " + styles.Hidden} />
+            {items}
+        </div>
     }
 }
- 
-BoilerplateComponent.propTypes = propTypes;
-BoilerplateComponent.defaultProps = defaultProps;
- 
-export default BoilerplateComponent;
+
+ResponsiveSidebar.defaultProps = {
+    menu: {
+        header: {
+            "type": "item",
+            "value": "Vilokan Labs",
+            "link": "/"
+        },
+        container: [
+            {
+                "type": "item",
+                "value": "Project",
+                "link": "/project"
+            },
+            {
+                "type": "submenu",
+                "value": "Issues",
+                "link": "",
+                "items": [
+                    {
+                        "type": "subitem",
+                        "value": "List",
+                        "link": "/issues"
+                    },
+                    {
+                        "type": "subitem",
+                        "value": "Boards",
+                        "link": "/boards"
+                    },
+                    {
+                        "type": "subitem",
+                        "value": "Labels",
+                        "link": "/labels"
+                    },
+                    {
+                        "type": "subitem",
+                        "value": "Service Desk",
+                        "link": "/service_desk"
+                    }
+                ]
+            },
+            {
+                "type": "item",
+                "value": "Members",
+                "link": "/members"
+            },
+        ],
+        footer: {
+            "type": "item",
+            "value": "<< Collapse Sidebar",
+            "link": ""
+        },
+    }
+};
+
+export default ResponsiveSidebar
