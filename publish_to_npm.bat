5af82815daaeb5e1ad049d0b10de6e1d6329db20
duke79 <pulkitsingh01@gmail.com>
2018-08-04 07:13:43 +0530

    publish | use np

--- publish_to_npm.bat
+++ publish_to_npm.bat
@@ -1,6 +1,10 @@
+@echo off
 REM http://jasonwatmore.com/post/2018/04/14/react-npm-how-to-publish-a-react-component-to-npm
 
-call npm install
-call npm run build
-call npm login
-call npm publish
+
+REM call npm install
+REM call npm run build
+REM call npm login
+REM call npm publish
+
+np
