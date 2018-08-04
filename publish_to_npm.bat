50aa803de9edb3bbdc1ae6403cc885e5c45fc35a
duke79 <pulkitsingh01@gmail.com>
2018-08-04 11:04:26 +0530

    trying build before publishing

--- publish_to_npm.bat
+++ publish_to_npm.bat
@@ -2,8 +2,8 @@
 REM http://jasonwatmore.com/post/2018/04/14/react-npm-how-to-publish-a-react-component-to-npm
 
 
-REM call npm install
-REM call npm run build
+call npm install
+call npm run build
 REM call npm login
 REM call npm publish
 
